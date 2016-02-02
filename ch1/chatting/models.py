be1e0f2cd035cf741ffe3b690989e0ee371c0354
박영환 <dudrnxps1@nate.com>
2016-02-02 15:32:14 +0900

    [박영환] (Member, Team 클래스 생성) - models.py

--- ch1/chatting/models.py
+++ ch1/chatting/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    m_id = models.CharField(max_length = 10, unique = True)
+    m_mail = models.EmailField
+    m_pass = models.CharField(max_length = 10)
+    
+    def __str__(self):
+        return self.m_id
+    
+class Team(models.Model):
+    t_name = models.CharField(max_length = 20, unique = true)
+    m_id = models.ManyToManyField(Member)
+    
+    def __str__(self):
+        return self.t_name
