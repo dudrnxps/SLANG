f3a3f9df02a972b3fd48dc6ef84e8fcff8322ef4
박영환 <dudrnxps1@nate.com>
2016-02-02 16:03:17 +0900

    [박영환] (Member 클래스, Email 칼럼 수정) - models.py *********************테이블 작성 완료*********************

--- ch1/chatting/models.py
+++ ch1/chatting/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Member(models.Model):
     m_id = models.CharField(max_length = 10, unique = True)
-    m_mail = models.EmailField
+    m_mail = models.EmailField(max_length =70)
     m_pass = models.CharField(max_length = 10)
     
     def __str__(self):
