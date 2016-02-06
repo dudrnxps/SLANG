15d0d4b70abfe0c836aaced72cce88d5e43aa39b
Moon Gwanghun <mkh9209@naver.com>
2016-02-07 02:57:18 +0900

    [문광훈] (templates 디렉토리 및 하위 디렉토리 추가, index 뷰 올림) - urls.py

---
+++ ch1/chatting/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns,url
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from chatting import views
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+    url(r'^$',TemplateView.as_view(template_name="chatting/index.html"),name="index"),
+    url(r'^Signup/$',TemplateView.as_view(template_name="chatting/Signup.html"),name="Signup"),
+    url(r'^Login/$',TemplateView.as_view(template_name="chatting/Login.html"),name="Login"),
+    url(r'^/$',TemplateView.as_view(template_name="js/cloud-carousel.1.0.5.js"),name="cloud-carousel"),
+)
--- ch1/chatting/views.py
+++ ch1/chatting/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views.generic import ListView
+from chatting.models import Membership, Team, File
 # Create your views here.
+
