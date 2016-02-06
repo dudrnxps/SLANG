87755c07a07a0404b73b71bd2803dcb9807b8dfa
박영환 <dudrnxps1@nate.com>
2016-02-06 19:41:43 +0900

    [박영환] (File 테이블 작성) - models.py

--- ch1/chatting/models.py
+++ ch1/chatting/models.py
@@ -3,16 +3,26 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Membership(models.Model):
-    m_id = models.CharField(max_length = 10, unique = True)
-    m_mail = models.EmailField(max_length =70)
-    m_pass = models.CharField(max_length = 10)
+    mem_username = models.CharField(max_length = 10)
+    mem_mail = models.EmailField(max_length =70)
+    mem_pass = models.CharField(max_length = 10)
     
     def __str__(self):
-        return self.m_id
+        return self.mem_username
     
 class Team(models.Model):
-    t_name = models.CharField(max_length = 20, unique = True)
-    m_id = models.ManyToManyField(Membership)
+    team_name = models.CharField(max_length = 20)
+    mem_username = models.ManyToManyField(Membership)
     
     def __str__(self):
-        return self.t_name
+        return self.team_name
+    
+class File(models.Model):
+    file_name = models.CharField(max_length = 100)
+    file_size = models.IntegerField(default = 0)
+    file_data = models.CharField(max_length = 30)
+    team_id = models.ForeignKey(Team)
+    membership_id = models.ForeignKey(Membership)
+    
+    def __str__(self):
+        return self.file_name
